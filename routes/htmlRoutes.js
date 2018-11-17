b010f7f582354e9ad92b2d15b7128e9e94355bb4
lcervantesm <cervants2802@gmail.com>
2018-11-17 13:02:39 -0600

    update

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,14 @@
 var db = require("../models");
-
+var firebase = require('firebase');
+var FirebaseApp = firebase.initializeApp({
+  apiKey: '<your-api-key>',
+  authDomain: '<your-auth-domain>',
+  databaseURL: '<your-database-url>',
+  projectId: '<your-cloud-firestore-project>',
+  storageBucket: '<your-storage-bucket>',
+  messagingSenderId: '<your-sender-id>'}
+);
+// ...
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
