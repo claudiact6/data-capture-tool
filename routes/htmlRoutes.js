8281d0a02d2a2c10afd711620a882b935f8f7155
Esteban Batman <41240399+esanchezvz@users.noreply.github.com>
2018-11-24 13:19:46 -0600

    Revert "html routes updated"
    This reverts commit f6d6f627c4791824c0a55e839a227f5bf2c16df6.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,21 +6,12 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     res.render("index");
   });
-
-  // Render login page
-  app.get("/login", function(req, res) {
-    res.render("login");
-  });
-
-  app.get("/dashboard", function(req, res) {
-    res.render("admin");
-  });
   // Load example page and pass in an example by id
-  app.get("/dashboard/:id", function(req, res) {
+  app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(
       dbExample
     ) {
-      res.render("admin", {
+      res.render("example", {
         example: dbExample
       });
     });
@@ -30,8 +21,6 @@ module.exports = function(app) {
   app.get("/register", function(req, res) {
     res.render("register");
   });
-
-  //Render Admin Dashboard
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
